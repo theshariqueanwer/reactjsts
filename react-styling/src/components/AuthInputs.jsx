8fee6928949fdc6aca86b08418eef0780b9649c1
SHARIQUE ANWER <theshariqueanwer>
2024-01-09 14:43:20 +0530

    vanila css are not scoped to componenets

--- react-styling/src/components/AuthInputs.jsx
+++ react-styling/src/components/AuthInputs.jsx
@@ -42,6 +42,7 @@ export default function AuthInputs() {
           />
         </p>
       </div>
+      <p>just as a some demo text</p>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
