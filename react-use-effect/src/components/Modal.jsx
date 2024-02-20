71fb6a620300e9656cbc127449cd2753761730c7
SHARIQUE ANWER <mdshariqueanwer01@gmail.com>
2024-02-20 10:49:26 +0530

    useeffect all

--- react-use-effect/src/components/Modal.jsx
+++ react-use-effect/src/components/Modal.jsx
@@ -1,26 +1,101 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+function Modal({ open, onClose, children }) {
+  // console.log('Modal Component Run');
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+  // if(open) {
+  //   dialog.current.showModal();
+  // } else {
+  //   dialog.current.close();
+  // }
+
+  useEffect(() => {
+    if(open) {
+      dialog.current.showModal();
+      // console.log('Modal Open');
+    } else {
+      dialog.current.close();
+      // console.log('Modal Close');
+    }
+  }, [open])
 
   return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
+    // <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog} onClose={onClose} >
+      {/* {children} */}
+      {open ? children : null}
     </dialog>,
     document.getElementById('modal')
   );
-});
+}
 
 export default Modal;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { forwardRef, useImperativeHandle, useRef } from 'react';
+// import { createPortal } from 'react-dom';
+
+// const Modal = forwardRef(function Modal({ children }, ref) {
+//   const dialog = useRef();
+
+//   useImperativeHandle(ref, () => {
+//     return {
+//       open: () => {
+//         dialog.current.showModal();
+//       },
+//       close: () => {
+//         dialog.current.close();
+//       },
+//     };
+//   });
+
+//   return createPortal(
+//     <dialog className="modal" ref={dialog}>
+//       {children}
+//     </dialog>,
+//     document.getElementById('modal')
+//   );
+// });
+
+// export default Modal;
