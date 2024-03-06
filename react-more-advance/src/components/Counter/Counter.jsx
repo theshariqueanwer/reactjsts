27ec350287f9758646d837a2b634e47af6a6e8ed
SHARIQUE ANWER <mdshariqueanwer01@gmail.com>
2024-03-06 23:24:02 +0530

    more advance commit in more

--- react-more-advance/src/components/Counter/Counter.jsx
+++ react-more-advance/src/components/Counter/Counter.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import { log } from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 function isPrime(number) {
   log(
@@ -13,10 +14,12 @@ function isPrime(number) {
     'other'
   );
   if (number <= 1) {
+    // console.log(number <= 1);
     return false;
   }
 
   const limit = Math.sqrt(number);
+  // console.log(limit);
 
   for (let i = 2; i <= limit; i++) {
     if (number % i === 0) {
@@ -27,19 +30,32 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+const Counter = memo(function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  // const initialCountIsPrime = isPrime(initialCount);  // this is done before
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
-  const [counter, setCounter] = useState(initialCount);
+  // useEffect(() => {
+  //   setCounterChanges([{value: initialCount, id: Math.random() * 1000}]);
+  // }, [initialCount])
 
-  function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
-  }
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([{value: initialCount, id: Math.random() * 1000}]);
 
-  function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
+
+  const handleDecrement = useCallback(function handleDecrement() {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{value: -1, id: Math.random() * 1000}, ...prevCounterChanges]);
+  }, []);
+
+  const handleIncrement = useCallback(function handleIncrement() {
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [{value: 1, id: Math.random() * 1000}, ...prevCounterChanges]);
+  }, []);
 
   return (
     <section className="counter">
@@ -51,11 +67,15 @@ export default function Counter({ initialCount }) {
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
-        <CounterOutput value={counter} />
+        {/* <CounterOutput value={counter} /> */}
+        <CounterOutput value={currentCounter} />
         <IconButton icon={PlusIcon} onClick={handleIncrement}>
           Increment
         </IconButton>
       </p>
+      <CounterHistory history={counterChanges} />
     </section>
   );
-}
+});
+
+export default Counter;
